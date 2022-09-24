fb10bb25ac2d098017f3178416a76a11738d04eb
Orlando N. Rodriguez <onovas05@gmail.com>
2022-09-23 21:01:25 -0400

    Initial commit

---
+++ app/src/main/java/com/orlandev/jetpackcomposetest/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.orlandev.jetpackcomposetest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.flowlayout.FlowMainAxisAlignment
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.SizeMode
+import com.orlandev.jetpackcomposetest.tests.TestNestedLazyScroll
+import com.orlandev.jetpackcomposetest.ui.theme.JetpackComposeTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                    TestNestedLazyScroll()
+
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/orlandev/jetpackcomposetest/tests/TestNestedLazyScroll.kt
@@ -0,0 +1,46 @@
+package com.orlandev.jetpackcomposetest.tests
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.flowlayout.FlowMainAxisAlignment
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.SizeMode
+
+
+@Composable
+fun TestNestedLazyScroll() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        item {
+            for (i in 1..10) {
+                Text(text = "Text $i", modifier = Modifier.padding(10.dp))
+            }
+        }
+        item {
+            val itemSize: Dp = (LocalConfiguration.current.screenWidthDp.dp / 2)
+            FlowRow(
+                mainAxisSize = SizeMode.Expand,
+                mainAxisAlignment = FlowMainAxisAlignment.SpaceBetween
+            ) {
+                for (i in 1..10) {
+                    Text(
+                        text = "Text $i",
+                        modifier = Modifier.size(itemSize),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
